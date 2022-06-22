d9ed6a381d1a1cccdd38294e8950467d8c01e56c
Vinicius Jara <viniciusfjara@gmail.com>
2022-06-21 23:30:49 -0300

    inicia implementação da sidebar

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,16 @@
-export function Lesson(){
-    return(
-        <></>
+export function Lesson() {
+    return (
+        <a href="#">
+            <span>Domingo • 20 de junho • 19h00</span>
+
+            <div>
+                <header>
+                    <span>Conteúdo Liberado</span>
+                    <span>AO VIVO</span>
+                </header>
+
+                <strong>Abertura do evento Ignite Lab</strong>
+            </div>
+        </a>
     )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,19 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar() {
     return (
-        <></>
+        <aside className="w-[348px] p-6 bg-gray-700 border-l border-gray-600">
+            <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+                Cronograma de aulas
+            </span>
+            <div className="flex flex-col gap-8">
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+            </div>
+        </aside>
     )
 }

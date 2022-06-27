e08533cd450440b4c0d62641b6a37d29354866a9
Vinicius Jara <viniciusfjara@gmail.com>
2022-06-27 18:34:34 -0400

    finalização do projeto com graphql codegen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
 
-const GET_LESSONS_QUERY = gql`
-    query {
-        lessons(orderBy: availableAt_ASC) {
-            id
-            slug
-            title
-            lessonType
-            availableAt
-        }
-    }
-`
 
-interface GetLessonsQueryResponse {
-    lessons: {
-        id: string;
-        title: string;
-        slug: string;
-        availableAt: string;
-        lessonType: 'live' | 'class'
-    }[]
-}
 
 export function Sidebar() {
-    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
-
+    const { data } = useGetLessonsQuery()
 
     return (
         <aside className="w-[348px] p-6 bg-gray-700 border-l border-gray-600">
@@ -36,7 +15,7 @@ export function Sidebar() {
             <div className="flex flex-col gap-8">
                 {data?.lessons.map(lesson => {
                     return (
-                        <Lesson 
+                        <Lesson
                             key={lesson.id}
                             title={lesson.title}
                             slug={lesson.slug}
